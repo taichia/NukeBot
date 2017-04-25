16e579453cec753e5af74c7b7c73ac9d4a65977f
Taichi Akiyama <taichia@andrew.cmu.edu>
2017-04-25 19:29:10 -0400

    syntax fixes

--- serial_complete.py
+++ serial_complete.py
@@ -2,6 +2,14 @@ import serial
 import sys
 import time
 
+
+# Change this for every sensor used, no way to automatically 
+# detect which sensors are being used, as there is no 
+# direct connection between the sensors and the computer and also
+# prob impossible even if there was a direct connection.
+laser_model = 'Laser Beam Pew Pew' 	
+inductive_model = 'Bongo-Lite'
+
 #The following line is for serial over GPIO
 #The port number needs to be changed every time you change computers/reboot(?)
 port1 = '/dev/tty.usbmodem1411'
@@ -12,23 +20,20 @@ ard = serial.Serial(port1,115200,timeout=5)
 laser = serial.Serial(port2, 9600, timeout=5)
 inductive = serial.Serial(port3, 9600, timeout=5)
 
-var = input("Press 1 to start: ")
-while var is not "1"
-	var = input("\nStart failed. Please type 1 to start: ")
+print ('Before starting, make sure you changed sensor model numbers.')
+print ('Currently, the laser sensor is: ' + laser_model)
+print ('The inductive sensor is: ' + inductive_model)
+var = input('Press 1 to start: ')
 
-print "\nStart sequence initiated, please do not touch anything\n"
+while var is not '1':
+	var = input('Start failed. Please type 1 to start: ')
+
+print ('Start sequence initiated, please do not touch anything')
 ard.flush()
 laser.flush()
 inductive.flush()
 
-# Change this for ever sensor used, no way to automatically 
-# detect which sensors are being used, as there is no 
-# direct connection between the sensors and the computer and also
-# prob impossible even if there was a direct connection.
-laser_model = "Laser Beam Pew Pew" 	
-inductive_model = "Bongo-Lite (I can't think of funny names)"
-
-file_extension = ".txt"
+file_extension = '.txt'
 laser_file_name = laser_model + file_extension
 inductive_file_name = inductive_model + file_extension
 
@@ -42,7 +47,7 @@ exit()
 
 
 def read_values(sensor):
-	send_message = "VAL"
+	send_message = 'VAL'
 	send_message = str(send_message)
 	sensor.write(send_message.encode())
 
