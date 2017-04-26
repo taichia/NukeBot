c34e14c7e3e6ccf2a1a794b34605d3659f08c435
Taichi Akiyama <taichia@andrew.cmu.edu>
2017-04-26 03:09:26 -0400

    FLUKE 45 doesn't work, need to convert to ADC shield

--- serial_complete.py
+++ serial_complete.py
@@ -2,6 +2,11 @@ import serial
 import sys
 import time
 
+def read_values(sensor):
+	send_message = 'VAL'
+	send_message = str(send_message)
+	sensor.write(send_message.encode())
+	return sensor.readline()
 
 # Change this for every sensor used, no way to automatically 
 # detect which sensors are being used, as there is no 
@@ -10,15 +15,23 @@ import time
 laser_model = 'Laser Beam Pew Pew' 	
 inductive_model = 'Bongo-Lite'
 
+# 200*10*6 should be max number of steps possible
+max_steps = 200*10*6
+
 #The following line is for serial over GPIO
 #The port number needs to be changed every time you change computers/reboot(?)
 port1 = '/dev/tty.usbmodem1411'
-port2 = '/dev/tty.usbmodem1412'
+port2 = '/dev/tty.usbserial-AK05DQ2K'
 port3 = '/dev/tty.usbmodem1413'
 
-ard = serial.Serial(port1,115200,timeout=5)
-laser = serial.Serial(port2, 9600, timeout=5)
-inductive = serial.Serial(port3, 9600, timeout=5)
+#ard = serial.Serial(port1,115200,timeout=5)
+laser = serial.Serial(port = port2,
+	baudrate = 9600, 
+	parity = serial.PARITY_NONE,
+	stopbits = serial.STOPBITS_ONE,
+	bytesize = serial.EIGHTBITS,
+	timeout=20)
+#inductive = serial.Serial(port3, 9600, timeout=5)
 
 print ('Before starting, make sure you changed sensor model numbers.')
 print ('Currently, the laser sensor is: ' + laser_model)
@@ -29,28 +42,28 @@ while var is not '1':
 	var = input('Start failed. Please type 1 to start: ')
 
 print ('Start sequence initiated, please do not touch anything')
-ard.flush()
-laser.flush()
-inductive.flush()
 
-file_extension = '.txt'
-laser_file_name = laser_model + file_extension
-inductive_file_name = inductive_model + file_extension
+laser.write('*IDN?\n'.encode())
 
-laser_file = open(laser_file_name, 'w')
-inductive_file_name = open(inductive_file_name, 'w')
+#ard.flush()
+
+print(laser.readline())
+
+#inductive.flush()
+
+#file_extension = '.txt'
+#laser_file_name = laser_model + file_extension
+#inductive_file_name = inductive_model + file_extension
+
+#laser_file = open(laser_file_name, 'w')
+#inductive_file_name = open(inductive_file_name, 'w')
 
 
+#for x in range(0, 10): 
+#	print (read_values(laser))
 
 
 exit()
 
 
-def read_values(sensor):
-	send_message = 'VAL'
-	send_message = str(send_message)
-	sensor.write(send_message.encode())
 
-	sensor.flush()
-
-	return sensor.readline()
